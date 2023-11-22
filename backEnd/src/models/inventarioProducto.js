35ed6ff06cfafdb8912d0f975c929745013df898
INTERWEB <bencito10@gmail.com>
2023-11-22 17:22:03 -0500

    refactor (back / models) : update models

--- backEnd/src/models/inventarioProducto.js
+++ backEnd/src/models/inventarioProducto.js
@@ -1,25 +1,25 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-	// defino el modelo
-	sequelize.define(
-		'INVENTARIO_PRODUCTO',
-		{
-			id_cantidad_producto: {
-				type: DataTypes.UUID,
-				defaultValue: DataTypes.UUIDV4,
-				primaryKey: true,
-			},
+  // defino el modelo
+  sequelize.define(
+    "INVENTARIO_PRODUCTO",
+    {
+      id_cantidad_producto: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
 
-			valor_inventario: {
-				type: DataTypes.INTEGER,
-				allowNull: false,
-			},
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
 
-			//InventarioProducto UUID [ref: > PRODUCTO.id_producto]
-			//  InventarioSucursal UUID [ref: > SUCURSAL.id_sucursal]
-		},
-		{ freezeTableName: true, timestamps: false }
-	);
+      //InventarioProducto UUID [ref: > PRODUCTO.id_producto]
+      //  InventarioSucursal UUID [ref: > SUCURSAL.id_sucursal]
+    },
+    { freezeTableName: true, timestamps: false }
+  );
 };
