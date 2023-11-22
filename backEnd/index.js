6f5ba88ff4ea3be42bc701f8d2fe031197a0798a
LucasAEscudero <lucasescudero5629@gmail.com>
2023-11-22 17:00:11 -0300

    feat(back / server): listening server
    Co-authored-by: David Castro Mora <david@castromora.lat>
    Co-authored-by: jhordeibys <jhordeibys@users.noreply.github.com>

---
+++ backEnd/index.js
@@ -0,0 +1,8 @@
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  conn.sync({ force: false });
+  console.log(`listening at ${PORT}`);
+});
