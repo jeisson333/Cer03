77bbdcb458717e4e1d424ba34d73f9f7ae9c7864
INTERWEB <bencito10@gmail.com>
2023-11-22 17:25:29 -0500

    refactor(back/src): little updates

--- backEnd/src/app.js
+++ backEnd/src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const routes = require("./routes/index.js");
+// const routes = require("./routes/index.js");
 const cors = require("cors");
 
 const server = express();
@@ -13,7 +13,7 @@ server.use(cookieParser());
 server.use(morgan("dev"));
 server.use(cors());
 
-server.use("/", routes);
+// server.use("/", routes);
 
 // Error catching endware.
 server.use((err, req, res, next) => {
