1f29f2d812129eedab7d9ad0dd9f32df9d87b2c4
INTERWEB <bencito10@gmail.com>
2023-11-24 13:18:03 -0500

    refactor(front/ SignIn): update SignIn style and navigate to home

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,30 +4,40 @@ import Home from "./pages/Home/Home";
 import Inventory from "./pages/Inventory/Inventory";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import RegisterForm from "./pages/RegisterForm/RegisterForm";
-import About from "./pages/About/About"
+import About from "./pages/About/About";
 import SignIn from "./pages/SignIn/SignIn";
-import Contact from "./pages/Contact/Contact"
+import Contact from "./pages/Contact/Contact";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Settings from "./pages/Settings/Settings";
 import NavBAr from "./components/NavBar/NavBar";
 
 function App() {
   const location = useLocation();
-  const isActive  = location.pathname === '/contact' || location.pathname === '/about' || location.pathname === '/landingPage' ||  location.pathname === '/' ;
+  // const [isActive, setIsActive] = useState(false)
+
+  const isActive =
+    location.pathname === "/contact" ||
+    location.pathname === "/about" ||
+    location.pathname === "/landingPage" ||
+    location.pathname === "/" ||
+    location.pathname === "/signIn";
+
+  const navClass = !isActive ? "siderBarPosition" : "prueba";
+
   return (
-    <div className="siderBarPosition">
-      {!isActive && <NavBAr/>}
+    <div className={navClass}>
+      {!isActive && <NavBAr />}
       <Routes>
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/products" element={<Inventory />} />
-      <Route path="/settings" element={<Settings />} />
-      <Route exact path="/" element={<LandingPage />} />
-      <Route path="/registerForm" element={<RegisterForm />} />
-      <Route path="/signIn" element={<SignIn />} />
-      <Route path="/about" element={<About/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-    </Routes>
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/products" element={<Inventory />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route exact path="/" element={<LandingPage />} />
+        <Route path="/registerForm" element={<RegisterForm />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
