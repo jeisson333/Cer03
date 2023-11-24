e783a700fa1db2b9168695f796cce9a2d9cffb25
LucasAEscudero <lucasescudero5629@gmail.com>
2023-11-24 10:49:31 -0300

    refactor(back / products route): filter attributes

--- backEnd/src/controllers/obtainAllProducts.js
+++ backEnd/src/controllers/obtainAllProducts.js
@@ -2,10 +2,22 @@ const { PRODUCTO } = require("../database/db");
 // const { Op } = require("sequelize");
 
 const obtainAllProducts = async (branch) => {
-  let products = await PRODUCTO.findAll({
+  let products = [];
+
+  let productsFound = await PRODUCTO.findAll({
     where: {
       producto_empresa: branch, //all products --> branch
     },
+  });
+
+  productsFound.forEach((product) => {
+    products.push({
+      id: product.id_producto,
+      nombre: product.nombre_producto,
+      valor_venta: product.valor_venta,
+      valor_compra: product.valor_compra,
+      tipo: product.tipo_producto,
+    });
   });
 
   return products;
