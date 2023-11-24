dc766767c35df7d07af31475607cbaa602baad83
jeisson333 <ljeiisonl@gmail.com>
2023-11-24 06:32:54 -0500

    refactor actualizacion  de estilo en el componente  Navbar

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -3,10 +3,54 @@ import styles from './Home.module.css';
 export default function Home(){
     return(
         <div >
-
-        
+            
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
         
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deleniti eos sit amet ab laborum maxime cumque nisi, quidem nam. Illum suscipit reprehenderit soluta dolor voluptate quia, incidunt debitis inventore.</p>
         </div>
     )
 }
