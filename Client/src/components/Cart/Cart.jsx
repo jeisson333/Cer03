6e1dff7470d75e80a28b8ad1192714b2840e6352
LucasAEscudero <lucasescudero5629@gmail.com>
2023-11-29 20:03:18 -0300

    feat(front / cart): cart window

--- Client/src/components/Cart/Cart.jsx
+++ Client/src/components/Cart/Cart.jsx
@@ -1,17 +1,71 @@
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 import ProdCart from "../ProdCart/ProdCart";
 
+import styles from "./Cart.module.css";
+
 const Cart = () => {
   const products = useSelector((state) => state.inCart);
-  console.log(products);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [quantity, setQuantity] = useState({});
+
+  useEffect(() => {
+    setTotalPrice(0);
+    products.forEach((product) => {
+      if (!quantity[product.PRODUCTO.id_producto])
+        setQuantity({ [product.PRODUCTO.id_producto]: 1 });
+    });
+  }, [products]);
+
+  // useEffect(() => {
+  //   setTotalPrice(0);
+  //   // console.log(totalPrice);
+  //   products.forEach((product, i) => {
+  //     if (!quantity[product.PRODUCTO.id_producto])
+  //       setQuantity({ ...quantity, [product.PRODUCTO.id_producto]: 1 });
+  //     // console.log(
+  //     //   totalPrice,
+  //     //   product.PRODUCTO.valor_venta,
+  //     //   quantity[product.PRODUCTO.id_producto]
+  //     // );
+  //     if (quantity[product.PRODUCTO.id_producto]) {
+  //       setTotalPrice(
+  //         totalPrice + product.PRODUCTO.valor_venta
+  //         *
+  //           quantity[product.PRODUCTO.id_producto]
+  //       );
+  //     }
+  //   });
+  // }, [products, quantity]);
+
+  const quantityInputHandler = (event) => {
+    setQuantity({
+      ...quantity,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <div>
-      {products?.map((product) => {
-        return (
-          <ProdCart key={product.PRODUCTO.id_producto} product={product} />
-        );
-      })}
+      <div>
+        {products?.map((product) => {
+          return (
+            <ProdCart
+              key={product.PRODUCTO.id_producto}
+              product={product}
+              quantity={quantity}
+              quantityInputHandler={quantityInputHandler}
+            />
+          );
+        })}
+      </div>
+      <div>
+        <p>Precio Total: {totalPrice}</p>
+        <button onClick={() => console.log("Elija el metodo de pago")}>
+          Comprar
+        </button>
+      </div>
     </div>
   );
 };
--- Client/src/components/ProdCart/ProdCart.jsx
+++ Client/src/components/ProdCart/ProdCart.jsx
@@ -1,19 +1,33 @@
-const ProdCart = ({ product }) => {
+import styles from "./ProdCart.module.css";
+
+const ProdCart = ({ product, quantity, quantityInputHandler }) => {
+  console.log(product);
+  console.log(quantity);
   return (
-    <div>
-      <img
-        src={product.PRODUCTO.image}
-        alt={product.PRODUCTO.nombre_producto}
-      />
-      <h2>{product.PRODUCTO.nombre_producto}</h2>
-      <label htmlFor={product.PRODUCTO.id_producto}>Stock: </label>
-      <input
-        type="number"
-        name={product.PRODUCTO.id_producto}
-        min={1}
-        max={product.stock}
-        placeholder="Stock"
-      />
+    <div className={styles.prodCart}>
+      <div className={styles.prodInfo}>
+        <img
+          src={product.PRODUCTO.image}
+          alt={product.PRODUCTO.nombre_producto}
+        />
+        <h2>{product.PRODUCTO.nombre_producto}</h2>
+      </div>
+      <div className={styles.quantity}>
+        <label htmlFor={product.PRODUCTO.id_producto}>Cantidad: </label>
+        <input
+          type="number"
+          name={product.PRODUCTO.id_producto}
+          value={quantity[product.PRODUCTO.id_producto]}
+          onChange={quantityInputHandler}
+          min={1}
+          max={product.stock}
+          placeholder={product.stock}
+        />
+        <div>
+          <p>Stock: {product.stock}</p>
+          <p>Precio: {product.PRODUCTO.valor_venta}</p>
+        </div>
+      </div>
     </div>
   );
 };
