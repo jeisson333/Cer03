193ca40c9fb0c1b036cfc70d86caf5d790c0f9ff
INTERWEB <bencito10@gmail.com>
2023-12-01 16:54:42 -0500

    fix(front/dependencies): remove deprecated dependencie

--- Client/src/pages/LandingPage/LandingPage.jsx
+++ Client/src/pages/LandingPage/LandingPage.jsx
@@ -1,47 +1,55 @@
-import React, { useState } from 'react';
-import { Fade } from 'react-reveal';
-import SlideShow from '../../components/SlideShow/SlideShow';
-import Advantages from '../../components/Advantages/Advantages';
-import Header from '../../components/Header/Header';
-import Testimonials from '../../components/Testimonials/Testimonials';
+import React, { useState } from "react";
+import { Fade } from "react-awesome-reveal";
+import SlideShow from "../../components/SlideShow/SlideShow";
+import Advantages from "../../components/Advantages/Advantages";
+import Header from "../../components/Header/Header";
+import Testimonials from "../../components/Testimonials/Testimonials";
 
 const LandingPage = () => {
-
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? testimonialsData.length - 1 : prevIndex - 1));
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? testimonialsData.length - 1 : prevIndex - 1
+    );
   };
 
   const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === testimonialsData.length - 1 ? 0 : prevIndex + 1));
+    setCurrentIndex((prevIndex) =>
+      prevIndex === testimonialsData.length - 1 ? 0 : prevIndex + 1
+    );
   };
 
   const testimonialsData = [
     {
       id: 1,
-      name: 'Juan Pérez',
-      quote: '¡Increíble experiencia! Me encanta la comunidad y la conexión que proporciona Cer03.',
+      name: "Juan Pérez",
+      quote:
+        "¡Increíble experiencia! Me encanta la comunidad y la conexión que proporciona Cer03.",
     },
     {
       id: 2,
-      name: 'María García',
-      quote: 'Cer03 ha cambiado la forma en que interactúo con mi vecindario. Estoy muy agradecida.',
+      name: "María García",
+      quote:
+        "Cer03 ha cambiado la forma en que interactúo con mi vecindario. Estoy muy agradecida.",
     },
     {
       id: 3,
-      name: 'María García',
-      quote: 'Cer03 ha cambiado la forma en que interactúo con mi vecindario. Estoy muy agradecida.',
+      name: "María García",
+      quote:
+        "Cer03 ha cambiado la forma en que interactúo con mi vecindario. Estoy muy agradecida.",
     },
     {
       id: 4,
-      name: 'María García',
-      quote: 'Cer03 ha cambiado la forma en que interactúo con mi vecindario. Estoy muy agradecida.',
+      name: "María García",
+      quote:
+        "Cer03 ha cambiado la forma en que interactúo con mi vecindario. Estoy muy agradecida.",
     },
     {
       id: 5,
-      name: 'María García',
-      quote: 'Cer03 ha cambiado la forma en que interactúo con mi vecindario. Estoy muy agradecida.',
+      name: "María García",
+      quote:
+        "Cer03 ha cambiado la forma en que interactúo con mi vecindario. Estoy muy agradecida.",
     },
   ];
 
@@ -58,19 +66,22 @@ const LandingPage = () => {
           <Fade delay={300}>
             <p className="text-gray-600 mb-4">
               Explora las posibilidades de tu comunidad con nosotros. En Cer03,
-              priorizamos las conexiones y el crecimiento conjunto sobre las transacciones comerciales.
+              priorizamos las conexiones y el crecimiento conjunto sobre las
+              transacciones comerciales.
             </p>
           </Fade>
           <Fade delay={600}>
             <p className="text-gray-600 mb-4">
-              Registra y comparte tus experiencias, eventos y recursos con otros miembros del barrio.
-              Lleva un control detallado de tus interacciones, desde colaboraciones hasta actividades
-              cotidianas, sin la presión de vender.
+              Registra y comparte tus experiencias, eventos y recursos con otros
+              miembros del barrio. Lleva un control detallado de tus
+              interacciones, desde colaboraciones hasta actividades cotidianas,
+              sin la presión de vender.
             </p>
           </Fade>
           <Fade delay={900}>
             <p className="text-gray-600">
-              Únete a nosotros para construir juntos un espacio comunitario vibrante y próspero.
+              Únete a nosotros para construir juntos un espacio comunitario
+              vibrante y próspero.
             </p>
           </Fade>
         </div>
@@ -83,7 +94,11 @@ const LandingPage = () => {
       <div className="w-full p-8 mt-8">
         <Advantages />
       </div>
-      <Testimonials currentIndex={currentIndex} handlePrev={handlePrev} handleNext={handleNext} />
+      <Testimonials
+        currentIndex={currentIndex}
+        handlePrev={handlePrev}
+        handleNext={handleNext}
+      />
     </div>
   );
 };
