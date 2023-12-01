0cff63dd71cc82b25807e68fc51088877feb4fcc
jeisson333 <ljeiisonl@gmail.com>
2023-12-01 01:33:52 -0500

    refactor (subcription) se actualizaron los estilos

--- Client/src/pages/Subscription/Subscription.jsx
+++ Client/src/pages/Subscription/Subscription.jsx
@@ -5,46 +5,71 @@ import "./Subscription.css";
 const Subscription = () => {
   const navigate = useNavigate();
 
-  const [subscriptionType, setSubscriptionType] = useState("free");
-  const [subscriptionMonths, setSubscriptionMonths] = useState(1);
-  const [selectedCurrency, setSelectedCurrency] = useState("COP");
+  const [subscriptionType, setSubscriptionType] = useState({
+    free: "Free",
+    basic: "Basic",
+    premium: "Premium",
+  });
+  const [subscriptionPay, setSubscriptionPay] = useState({
+    free: 0,
+    basic: 100,
+    premium: 300,
+  });
+  const [selectedCurrency, setSelectedCurrency] = useState([
+    "COP",
+    "ARG",
+    "PER",
+    "USD",
+  ]);
+  let [subscriptionMonths, setSubscriptionMonths] = useState(1);
+  const [isAnnual, setIsAnnual] = useState(false);
 
-  const subscriptionOptions = [
-    { type: "Free", price: 0 },
-    { type: "Basic", price: 10000 },
-    { type: "Premium", price: 50000 },
-  ];
-
-  const handleTypeChange = (event) => {
-    setSubscriptionType(event.target.value);
+  const handleCheckboxChange = () => {
+    setIsAnnual(!isAnnual);
   };
 
-  const handleMonthsChange = (event) => {
-    setSubscriptionMonths(event.target.value);
+  const calculateSubscriptionPrice = (type) => {
+    const basePrice = subscriptionPay[type];
+    return isAnnual ? basePrice * 12 : basePrice;
   };
 
-  const handleCurrencyChange = (event) => {
-    setSelectedCurrency(event.target.value);
-  };
+  const handleButtonClick = (type) => {
+    const country = document.getElementById("country").value;
+    const isChecked = isAnnual;
+    const name = subscriptionType[type];
+    const price = subscriptionPay[type];
 
-  const handlePaymentClick = () => {
-    const selectedSubscription = subscriptionOptions.find(
-      (option) => option.type === subscriptionType
-    );
+    if (isChecked) {
+      subscriptionMonths = 12;
+    }
     navigate(
-      `/subscription/checkout/?subscription=${subscriptionType}&totalPay=${selectedSubscription.price}&currency_id=${selectedCurrency}&quantity=${subscriptionMonths}`
+      `/subscription/checkout/?subscription=${name}&totalPay=${price}&currency_id=${country}&quantity=${subscriptionMonths}`
     );
   };
 
   return (
     <div className="container">
+      <div>
+        <select id="country">
+          {selectedCurrency.map((p, i) => (
+            <option key={i} value={p}>
+              {p}
+            </option>
+          ))}
+        </select>
+      </div>
       <header>
         <h1>Nuestros Precios</h1>
         <div className="toggle">
           <label>Anual</label>
           <div className="toggle-btn">
-            <input type="checkbox" id="checkbox" className="checkbox" />
-            <label form="checkbox" className="sub" id="suv">
+            <input
+              type="checkbox"
+              id="checkbox"
+              className="checkbox"
+              onChange={handleCheckboxChange}
+            />
+            <label htmlFor="checkbox" className="sub" id="suv">
               <div className="circle"></div>
             </label>
           </div>
@@ -55,110 +80,60 @@ const Subscription = () => {
       <div className="cards">
         <div className="card shadow">
           <ul>
-            <li className="pack">Free</li>
-            <li id="free" className="priceBottomBar">
-              $0
+            <li className="pack">{subscriptionType.free}</li>
+            <li id="free" className="price bottonBar">
+              ${calculateSubscriptionPrice("free")}
             </li>
             <li className="bottonBar">1 funcion</li>
             <li className="bottonBar">2 funcion</li>
+            <li className="bottonBar">3 funcion</li>
             <li>
-              <button className="btn">Lo quiero</button>
+              <button className="btn" onClick={() => handleButtonClick("free")}>
+                Lo quiero
+              </button>
             </li>
           </ul>
         </div>
         <div className="card active">
           <ul>
-            <li className="pack">Basic</li>
-            <li id="free" className="priceBottomBar">
-              $100
+            <li className="pack">{subscriptionType.basic}</li>
+            <li id="free" className="price bottonBar">
+              ${calculateSubscriptionPrice("basic")}
             </li>
             <li className="bottonBar">1 funcion</li>
             <li className="bottonBar">2 funcion</li>
             <li className="bottonBar">3 funcion</li>
-            <li className="bottonBar">4 funcion</li>
             <li>
-              <button className="btn active-btn">Lo quiero</button>
+              <button
+                className="btn active-btn"
+                onClick={() => handleButtonClick("basic")}
+              >
+                Lo quiero
+              </button>
             </li>
           </ul>
         </div>
         <div className="card shadow">
           <ul>
-            <li className="pack">Profesional</li>
-            <li id="free" className="priceBottomBar">
-              $300
+            <li className="pack">{subscriptionType.premium}</li>
+            <li id="free" className="price bottonBar">
+              ${calculateSubscriptionPrice("premium")}
             </li>
             <li className="bottonBar">1 funcion</li>
             <li className="bottonBar">2 funcion</li>
             <li className="bottonBar">3 funcion</li>
-            <li className="bottonBar">4 funcion</li>
-            <li className="bottonBar">5 funcion</li>
             <li>
-              <button className="btn">Lo quiero</button>
+              <button
+                className="btn"
+                onClick={() => handleButtonClick("premium")}
+              >
+                Lo quiero
+              </button>
             </li>
           </ul>
         </div>
       </div>
     </div>
-    // <div>
-    //   <h2>Información de la suscripción</h2>
-    //   <label htmlFor="currency">Seleccionar Moneda:</label>
-    //   <select
-    //     id="currency"
-    //     value={selectedCurrency}
-    //     onChange={handleCurrencyChange}
-    //   >
-    //     {["COP", "ARS", "PER", "USD"].map((currency) => (
-    //       <option key={currency} value={currency}>
-    //         {currency}
-    //       </option>
-    //     ))}
-    //   </select>
-
-    //   <table>
-    //     <thead>
-    //       <tr>
-    //         <th>Tipo</th>
-    //         <th>Precio</th>
-    //       </tr>
-    //     </thead>
-    //     <tbody>
-    //       {subscriptionOptions.map((option) => (
-    //         <tr key={option.type}>
-    //           <td>{option.type}</td>
-    //           <td>{option.price}</td>
-    //         </tr>
-    //       ))}
-    //     </tbody>
-    //   </table>
-
-    //   <label htmlFor="subscriptionType">Seleccionar Tipo de Suscripción:</label>
-    //   <select
-    //     id="subscriptionType"
-    //     value={subscriptionType}
-    //     onChange={handleTypeChange}
-    //   >
-    //     {subscriptionOptions.map((option) => (
-    //       <option key={option.type} value={option.type}>
-    //         {option.type}
-    //       </option>
-    //     ))}
-    //   </select>
-
-    //   <label htmlFor="subscriptionMonths">Seleccionar Número de Meses:</label>
-    //   <select
-    //     id="subscriptionMonths"
-    //     value={subscriptionMonths}
-    //     onChange={handleMonthsChange}
-    //   >
-    //     {[...Array(12).keys()].map((month) => (
-    //       <option key={month + 1} value={month + 1}>
-    //         {month + 1}
-    //       </option>
-    //     ))}
-    //   </select>
-
-    //   <button onClick={handlePaymentClick}>Ir a Pagar</button>
-    // </div>
   );
 };
 
