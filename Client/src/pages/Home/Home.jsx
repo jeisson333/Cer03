d42aba378537f005469973a2d3de86ca417bd6f3
LucasAEscudero <lucasescudero5629@gmail.com>
2023-12-03 19:39:18 -0300

    refactor(front / navbar): add navbar by roles

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "./Home.module.css";
 const url = import.meta.env.VITE_BASE_URL;
-import { NavLink, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Cookies from "universal-cookie";
 const cookies = new Cookies();
 
@@ -16,9 +16,9 @@ export default function Home() {
           id: idBranch,
         });
         setSales(data.data);
-        sales.forEach((sale) => {
-          console.log(Object.keys(sale));
-        });
+        // sales.forEach((sale) => {
+        //   console.log(Object.keys(sale));
+        // });
       } catch (error) {
         throw Error(error.message);
       }
@@ -29,10 +29,6 @@ export default function Home() {
     <div className={styles.container}>
       <div className={`${styles.movimientos}`}>
         <h2>Movimientos</h2>
-
-        <NavLink className={styles.buttonNv} to="/newsales">
-          Nueva venta
-        </NavLink>
       </div>
       <div className={styles.divider}></div>
       <table className={styles.productTable}>
