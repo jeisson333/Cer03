db7ac40173642c1be2bd60c9a29660f57b9f8124
Ddeltoro97 <davison09.ddt@gmail.com>
2023-12-03 18:26:28 -0500

    Responsive cart

--- Client/src/pages/NewSales/NewSales.jsx
+++ Client/src/pages/NewSales/NewSales.jsx
@@ -64,24 +64,7 @@ const NewSales = () => {
   );
 };
 
-// return (
-//   <div className={Style.divMain}>
-//     <div className={Style.holder}>
-//     <Filters
-//       conditions={conditions}
-//       setConditions={setConditions}
-//       sucursales={sucursales}
-//       totalPages={totalPages}
-//       handlerChange={handlerChange}
-//       handlerSubmit={handlerSubmit}
-//     />
-//     <Products />
-//     </div>
-//     <Cart />
 
-
-//   </div>
-// );
 
 
 export default NewSales;
