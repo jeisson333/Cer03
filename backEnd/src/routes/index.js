bafee0ed10e398d280fdce0cac6ad57af603e75b
INTERWEB <bencito10@gmail.com>
2023-12-03 13:36:30 -0500

    fix(back/database update): remove dev mode

--- backEnd/src/routes/index.js
+++ backEnd/src/routes/index.js
@@ -1,19 +1,19 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-const routeAuth = require('./routeAuth.js');
-const routeEmpresa = require('./routeEmpresa.js');
-const routeVentas = require('./routeVentas.js');
-const routeVendedor = require('./routeVendedor.js');
-const productsRouter = require('./productsRouter.js');
-const routeSucursales = require('./routeSucursales.js');
-const routeSaveJsonInDB = require('./saveJsonInDBRouter.js');
-const routeCatalogos = require('./routeCatalogo.js');
+const routeAuth = require("./routeAuth.js");
+const routeEmpresa = require("./routeEmpresa.js");
+const routeVentas = require("./routeVentas.js");
+const routeVendedor = require("./routeVendedor.js");
+const productsRouter = require("./productsRouter.js");
+const routeSucursales = require("./routeSucursales.js");
+//const routeSaveJsonInDB = require('./saveJsonInDBRouter.js');
+const routeCatalogos = require("./routeCatalogo.js");
 
-const routePaymentGateways = require('./routePaymentGateways.js');
-const routeSales = require('./routePaymentSales.js');
-const { verifyToken } = require('../middlewares/authJWT.js');
+const routePaymentGateways = require("./routePaymentGateways.js");
+const routeSales = require("./routePaymentSales.js");
+const { verifyToken } = require("../middlewares/authJWT.js");
 
 const router = Router();
 
@@ -24,9 +24,9 @@ router.use("/vendedor", verifyToken, routeVendedor);
 router.use("/ventas", routeVentas);
 router.use("/products", productsRouter);
 router.use("/sucursales", routeSucursales);
-router.use("/database", routeSaveJsonInDB);
+//router.use("/database", routeSaveJsonInDB);
 router.use("/catalogos", routeCatalogos);
-router.use('/paymentGateways', routePaymentGateways);
-router.use('/paymentSales', routeSales);
+router.use("/paymentGateways", routePaymentGateways);
+router.use("/paymentSales", routeSales);
 
 module.exports = router;
