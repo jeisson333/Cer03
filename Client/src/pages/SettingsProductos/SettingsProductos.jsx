316d5ea6f1323066c2eecd2e4d89d753de7055b7
Jose Luis Diaz Cardoza <joselo99_dc@hotmail.com>
2023-12-09 23:10:06 -0300

    Feat- (Front/Settings) new product configurations

---
+++ Client/src/pages/SettingsProductos/SettingsProductos.jsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import Style from "./settingsProductos.module.css";
+
+const SettingsProductos = () => {
+  const navigate = useNavigate();
+
+  const navigateToDeleteProductos = () => {
+    navigate("/deleteProduct");
+  };
+
+  return (
+    <div>
+      <button className={Style.Btn} onClick={navigateToDeleteProductos}>
+        Eliminar
+      </button>
+      <button className={Style.Btn}>Actualizar Inventario</button>
+    </div>
+  );
+};
+export default SettingsProductos;
