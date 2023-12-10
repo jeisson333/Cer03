6c7f63df2eb4a4e0e7e0d268046871625ea86282
Jose Luis Diaz Cardoza <joselo99_dc@hotmail.com>
2023-12-09 23:08:56 -0300

    Refactor- (Front/Settings) new organization

--- Client/src/pages/Settings/Settings.jsx
+++ Client/src/pages/Settings/Settings.jsx
@@ -4,32 +4,35 @@ import Style from "./Settings.module.css";
 import FormVendedor from "../../components/FormVendedor/FormVendedor";
 import Cookies from "universal-cookie";
 const cookies = new Cookies();
+import { useNavigate } from "react-router-dom";
 
 const Settings = () => {
   const url = import.meta.env.VITE_BASE_URL;
   const { idBranch } = cookies.get("auth");
+  const navigate = useNavigate();
+
+  const navigateToSettingsProductos = () => {
+    navigate("/settingsProductos");
+  };
+  const navigateToFormVendedor = () => {
+    navigate("/newSeller");
+  };
 
   useEffect(() => {}, []);
 
   return (
     <div>
       <div>
-        <button className={Style.Btn}>Vendedores</button>
-        <button>Producto</button>
-      </div>
-      <div>
-        <FormVendedor />
+        <button className={Style.Btn} onClick={navigateToFormVendedor}>
+          Vendedores
+        </button>
+        <button className={Style.Btn} onClick={navigateToSettingsProductos}>
+          Producto
+        </button>
+        <button className={Style.Btn}>Sucursales</button>
       </div>
     </div>
   );
-
-  // return (
-  //   <div>
-  //     {sucursales.map((sucursal, index) => (
-  //       <h3 key={index}>{sucursal?.nombre_sucursal}</h3>
-  //     ))}
-  //   </div>
-  // );
 };
 
 export default Settings;
