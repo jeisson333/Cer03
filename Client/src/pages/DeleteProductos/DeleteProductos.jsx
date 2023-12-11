bc98231825d83bcaeff1ee9d815fd74b0fbeb458
INTERWEB <bencito10@gmail.com>
2023-12-11 10:46:36 -0500

    fix(front/Delete product): fix bug and add filters

--- Client/src/pages/DeleteProductos/DeleteProductos.jsx
+++ Client/src/pages/DeleteProductos/DeleteProductos.jsx
@@ -4,7 +4,7 @@ import { getProducts, getSucursales } from "../../redux/actions";
 import { deleteProduct } from "../../redux/actions";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import axios from "axios";
+import Filters from "../../components/Filters/Filters";
 import Swal from "sweetalert2";
 import Cookies from "universal-cookie";
 const cookies = new Cookies();
@@ -13,10 +13,15 @@ const DeleteProductos = () => {
   const dispatch = useDispatch();
   const { idBranch, branch } = cookies.get("auth");
   const [products, setProducts] = useState([]);
+  const totalPages = useSelector((state) => state.totalPages);
   const sucursales = useSelector((state) => state.sucursales);
+  const [search, setSearch] = useState("");
   const [conditions, setConditions] = useState({
     sucursal: branch ? branch : sucursales[0]?.nombre_sucursal,
+    page: 1,
+    page_size: 15,
   });
+  const [flagDeleteProduct, setFlagDeleteProduct] = useState(false);
 
   useEffect(() => {
     dispatch(getSucursales(idBranch));
@@ -32,9 +37,9 @@ const DeleteProductos = () => {
         }
       });
     }
-  }, [conditions]);
+  }, [conditions, flagDeleteProduct]);
 
-  const handleDeleteProduct = async (id_producto) => {
+  const handleDeleteProduct = (id_producto) => {
     Swal.fire({
       title: "¿Estás seguro?",
       text: "¡No podrás revertir esto!",
@@ -43,7 +48,7 @@ const DeleteProductos = () => {
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
       confirmButtonText: "Sí, borrarlo",
-    }).then((result) => {
+    }).then(async (result) => {
       if (result.isConfirmed) {
         Swal.fire({
           title: "Deleted!",
@@ -51,58 +56,79 @@ const DeleteProductos = () => {
           icon: "success",
         });
         // Si el usuario ha confirmado, ejecuta la acción de borrar el producto
-        dispatch(deleteProduct(id_producto, branch));
-        setProducts({});
+        await dispatch(deleteProduct(id_producto, branch));
+        setFlagDeleteProduct(!flagDeleteProduct);
       }
     });
   };
+  const handlerChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const handlerSubmit = () => {
+    setConditions({
+      ...conditions,
+      name: search,
+    });
+  };
+
   return (
     <div>
-      <div>
+      <div className="flex justify-center">
         <h2>Productos</h2>
       </div>
-      <table className="table-auto border-collapse border border-blue-800">
-        <thead>
-          <tr>
-            <th className="border border-blue-600 p-3">Imagen</th>
-            <th className="border border-blue-600 p-3">Nombre</th>
-            <th className="border border-blue-600 p-3">Tipo</th>
-            <th className="border border-blue-600 p-3">Sucursal</th>
-            <th className="border border-blue-600 p-3">Eliminar</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product, index) => (
-            <tr key={index}>
-              <td className="border border-blue-600 p-3">
-                <img
-                  src={product?.PRODUCTO?.image}
-                  alt={product?.PRODUCTO?.nombre_producto}
-                  className="w-20 h-20 rounded-lg object-cover"
-                />
-              </td>
-              <td className="border border-blue-600 p-3">
-                {product?.PRODUCTO?.nombre_producto}
-              </td>
-              <td className="border border-blue-600 p-3">
-                {product?.PRODUCTO?.CATALOGO_UNIVERSAL?.nombre_catalogo}
-              </td>
-              <td className="border border-blue-600 p-3">
-                {product?.SUCURSAL.nombre_sucursal}
-              </td>
-              <td className="border border-blue-600 p-3">
-                <button
-                  onClick={() =>
-                    handleDeleteProduct(product?.PRODUCTO?.id_producto)
-                  }
-                >
-                  <FontAwesomeIcon icon={faTrash} />
-                </button>
-              </td>
+      <Filters
+        conditions={conditions}
+        setConditions={setConditions}
+        sucursales={sucursales}
+        totalPages={totalPages}
+        handlerChange={handlerChange}
+        handlerSubmit={handlerSubmit}
+      />
+      <div className="flex justify-center">
+        <table className="table-auto border-collapse border border-blue-800">
+          <thead>
+            <tr>
+              <th className="border border-blue-600 p-3">Imagen</th>
+              <th className="border border-blue-600 p-3">Nombre</th>
+              <th className="border border-blue-600 p-3">Tipo</th>
+              <th className="border border-blue-600 p-3">Sucursal</th>
+              <th className="border border-blue-600 p-3">Eliminar</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {products?.map((product, index) => (
+              <tr key={index}>
+                <td className="border border-blue-600 p-3">
+                  <img
+                    src={product?.PRODUCTO?.image}
+                    alt={product?.PRODUCTO?.nombre_producto}
+                    className="w-20 h-20 rounded-lg object-cover"
+                  />
+                </td>
+                <td className="border border-blue-600 p-3">
+                  {product?.PRODUCTO?.nombre_producto}
+                </td>
+                <td className="border border-blue-600 p-3">
+                  {product?.PRODUCTO?.CATALOGO_UNIVERSAL?.nombre_catalogo}
+                </td>
+                <td className="border border-blue-600 p-3">
+                  {product?.SUCURSAL.nombre_sucursal}
+                </td>
+                <td className="border border-blue-600 p-3">
+                  <button
+                    onClick={() =>
+                      handleDeleteProduct(product?.PRODUCTO?.id_producto)
+                    }
+                  >
+                    <FontAwesomeIcon icon={faTrash} />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
