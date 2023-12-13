b5b6b6a736698365d2df3733952a60794c53a616
LucasAEscudero <lucasescudero5629@gmail.com>
2023-12-13 17:28:02 -0300

    fix(back / developersRoute): fix a problem with body values

--- backEnd/src/controllers/controllerDevelopers.js
+++ backEnd/src/controllers/controllerDevelopers.js
@@ -1,43 +1,42 @@
-const { DEVELOPER } = require('../database/db');
-const { Op } = require('sequelize');
+const { DEVELOPER } = require("../database/db");
+const { Op } = require("sequelize");
 const JWT_DEVELOPERS = process.env.JWT_DEVELOPERS;
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const singUpDeveloper = async ({ email, password }) => {
-	console.log(email, password, 'controllerrrrr');
-	const create = await DEVELOPER.findOne({
-		where: { email: email },
-	});
+  const create = await DEVELOPER.findOne({
+    where: { email: email },
+  });
 
-	if (create) return 'este usuario ya existe';
+  if (create) return "este usuario ya existe";
 
-	await DEVELOPER.create({
-		email: email,
-		password: password,
-	});
-	return 'usuario creado con exito!';
+  await DEVELOPER.create({
+    email: email,
+    password: password,
+  });
+  return "usuario creado con exito!";
 };
 
 const singInDeveloper = async ({ email, password }) => {
-	const found = DEVELOPER.findOne({
-		where: {
-			[Op.and]: [{ email: email }, { password: password }],
-		},
-	});
+  const found = await DEVELOPER.findOne({
+    where: {
+      [Op.and]: [{ email: email }, { password: password }],
+    },
+  });
 
-	if (found) {
-		const token = jwt.sign(
-			{
-				role: 'developer',
-				exp: Date.now() / 1000 + 60 * 1440,
-			},
-			JWT_DEVELOPERS
-		);
-		return token;
-	}
+  if (found) {
+    const token = jwt.sign(
+      {
+        role: "developer",
+        exp: Date.now() / 1000 + 60 * 1440,
+      },
+      JWT_DEVELOPERS
+    );
+    return token;
+  }
 };
 
 module.exports = {
-	singUpDeveloper,
-	singInDeveloper,
+  singUpDeveloper,
+  singInDeveloper,
 };
--- backEnd/src/handlers/handlersDevelopers.js
+++ backEnd/src/handlers/handlersDevelopers.js
@@ -1,41 +1,42 @@
 const {
-	singUpDeveloper,
-	singInDeveloper,
-} = require('../controllers/controllerDevelopers');
+  singUpDeveloper,
+  singInDeveloper,
+} = require("../controllers/controllerDevelopers");
 
 //const { singIn } = require('../controllers/controllerAuth.js');
 
 const postSingUpDevelopersHandler = async (req, res) => {
-	try {
-		const { email, password } = req.body;
-		const body = req.body;
+  try {
+    const { email, password } = req.body;
+    const body = req.body;
 
-		console.log(body, 'handlerrrrr');
-		const complete = await singUpDeveloper({ email, password });
+    console.log(body, "handlerrrrr");
+    const complete = await singUpDeveloper({ email, password });
 
-		if (complete === 'este usuario ya existe') {
-			return res
-				.status(400)
-				.json({ error: 'este usuario ya se encuentra registrado' });
-		}
+    if (complete === "este usuario ya existe") {
+      return res
+        .status(400)
+        .json({ error: "este usuario ya se encuentra registrado" });
+    }
 
-		return res.status(200).json({ message: complete });
-	} catch (error) {
-		return res.status(400).json({ error: error.message });
-	}
+    return res.status(200).json({ message: complete });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
 };
 
 const postSingInDevelopersHandler = async (req, res) => {
-	try {
-		const { email, password } = req.body;
-		const response = await singInDeveloper(email, password);
-		res.status(200).json(response);
-	} catch (error) {
-		res.status(400).json({ error: error.message });
-	}
+  try {
+    const { email, password } = req.body;
+
+    const response = await singInDeveloper({ email, password });
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = {
-	postSingUpDevelopersHandler,
-	postSingInDevelopersHandler,
+  postSingUpDevelopersHandler,
+  postSingInDevelopersHandler,
 };
